990b3d0e8680b217495ab04fd851c3517077e900
Hitarth <hitarthchudgar@gmail.com>
2021-01-17 23:56:39 -0500

    Simple live feed back

--- chapter 6/app.js
+++ chapter 6/app.js
@@ -1,34 +1,44 @@
 const form = document.querySelector('.signup-form');
 const feedback = document.querySelector('.feedback')
-// const username = document.querySelector('#username')
+const username = document.querySelector('#username')
 form.addEventListener('submit', e => {
     e.preventDefault();
 
     //validation
     const usernamenew = form.username.value;
     const userPattern = /^[a-z]{6,12}/;
-    let result = pattern.test(usernamenew);
-    if(result){
+    let result = userPattern.test(usernamenew);
+    if (result) {
         feedback.textContent = 'You are good to go!'
         feedback.style.color = 'green';
-    }else{
+    } else {
         feedback.textContent = 'Username should be between 6-12 characters long'
         feedback.style.color = 'red';
     }
 })
 
-//testing regEx
-const username = 'hitarth';
-const pattern = /^[a-z]{6,}$/
+//live feedback
+form.username.addEventListener('keyup', e => {
+    const userPattern = /^[a-z]{6,12}/;
+    if (userPattern.test(e.target.value)) {
+        form.username.setAttribute('class', 'success');
+    } else {
+        form.username.setAttribute('class', 'error');
+    }
+})
 
-let result = pattern.test(username); //returns boolean
-console.log(result);
+// //testing regEx
+// const username = 'hitarth';
+// const pattern = /^[a-z]{6,}$/
 
-if(result){
-    console.log('regEx passed');
-}else{
-    console.log('regEx failed');
-}
+// let result = pattern.test(username); //returns boolean
+// console.log(result);
 
-let reslocation = username.search(pattern);
-console.log(reslocation);
+// if (result) {
+//     console.log('regEx passed');
+// } else {
+//     console.log('regEx failed');
+// }
+
+// let reslocation = username.search(pattern);
+// console.log(reslocation);
