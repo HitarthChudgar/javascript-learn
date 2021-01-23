03f0f2cfb30d0952b037286676b31de98fd5a26b
Hitarth <hitarthchudgar@gmail.com>
2021-01-23 01:06:27 -0500

    Timestamps more in depth

--- app.js
+++ app.js
@@ -16,3 +16,20 @@ console.log('timestamp:', time.getTime());
 console.log(time.toDateString());
 console.log(time.toTimeString());
 console.log(time.toLocaleString());
+
+//more about timestamps
+const before = new Date('February 1 2019 7:30:59');
+const now = new Date();
+
+const diff = now.getTime() - before.getTime();
+console.log(diff);
+
+const mins = Math.round(diff / 100 / 60);
+const hours = Math.round(mins / 60);
+const days = Math.round(hours / 24);
+
+console.log(`the blog was written ${days} ago`);
+
+//convverting time stamps into date objects
+const timestamp = 1647897598;
+console.log(new Date(timestamp));
