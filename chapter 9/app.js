e55a6403ed609019929ebfa50d370455bb308e1c
Hitarth <hitarthchudgar@gmail.com>
2021-01-24 23:21:48 -0500

    Different stages of XML HTTP Request

--- chapter 9/app.js
+++ chapter 9/app.js
@@ -8,3 +8,16 @@ setTimeout(() => {
 
 console.log(3);
 console.log(4);
+
+//making HTTP requests
+const request = new XMLHttpRequest();
+
+request.addEventListener('readystatechange', ()=>{
+    // console.log(request, request.readyState);
+    if(request.readyState === 4){ //different states of the xmlhttp request
+        console.log(request.responseText);
+    }
+})
+
+request.open('GET','https://jsonplaceholder.typicode.com/todos/'); //setting up the request
+request.send(); //sending the request
