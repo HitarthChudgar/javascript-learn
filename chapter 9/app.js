c56a7c3a7674ec15c5586e081b707e38b8aa84be
Hitarth <hitarthchudgar@gmail.com>
2021-01-24 22:58:57 -0500

    Basics of ASYNC

---
+++ chapter 9/app.js
@@ -0,0 +1,10 @@
+console.log(1);
+console.log(2);
+
+//simple representation of a callback function
+setTimeout(() => {
+    console.log('callbackfunction fired');
+}, 2000)
+
+console.log(3);
+console.log(4);
